175b487486ad5c2ef901e86d887e4f2af0411366
klimprakhon <klimprakhon@gmail.com>
2025-10-01 17:17:12 +0700

    feat(docker-compose): separate lab into into each services

--- app/src/index.ts
+++ app/src/index.ts
@@ -2,34 +2,34 @@ import express from "express";
 
 const app = express();
 
-let unhealthy = false;
-
 app.get("/", (_req, res) => {
   res.send("Hello World!");
 });
 
-// Simple health check endpoint
+// Healthcheck as Readiness Probe
 app.get("/health", (_req, res) => {
-  if (unhealthy) {
-    res.status(500).json({ error: "Unhealthy" });
-  } else {
-    res.json({ status: "OK" });
-  }
+  res.json({ status: "OK" });
 });
 
 app.get("/unhealthy", (_req, res) => {
-  unhealthy = true;
-  res.json({ status: "App will go unhealthy soon" });
+  res.status(500).json({ error: "Unhealthy" });
 });
 
-setInterval(() => {
-  if (unhealthy) {
-    console.log("Exiting because of unhealthy state...");
-    process.exit(1); // non-zero exit -> restart policy triggers
-  }
-}, 5000); // 5 seconds to go crash
+app.get("/done", (_req, res) => {
+  res.json({ message: "Shutting down in 5 seconds with exit code 0" });
+  setTimeout(() => {
+    process.exit(0);
+  }, 5000);
+});
 
-const port = 8000;
+app.get("/fail", (_req, res) => {
+  res.json({ message: "Crashing in 5 seconds with exit code 1" });
+  setTimeout(() => {
+    process.exit(1); // non-zero exit -> restart policy triggers
+  }, 5000);
+});
+
+const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
 });
